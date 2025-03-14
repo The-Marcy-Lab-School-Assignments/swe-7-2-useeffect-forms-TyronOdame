35acecf9052d9ccdceaedc9f4170edfa2103db08
Tyron <tyronodame5@gmail.com>
2025-03-13 22:50:58 -0400

    giphy Adapters is complete

--- app/src/adapters/giphyAdapters.js
+++ app/src/adapters/giphyAdapters.js
@@ -5,16 +5,33 @@ TODO:
 - Import the API Key from your config.js file
 - Complete each adapter function to fetch from the trending/ and search/ endpoints. See the README for the complete endpoint URLs that you will be fetching from.
 */
-import { handleFetch } from './handleFetch.js'
+import API_KEY from './config.js';
+import { handleFetch } from './handleFetch.js';
 
-const baseUrl = 'https://api.giphy.com/v1/gifs'
+const baseUrl = `https://api.giphy.com/v1/gifs`;
 
 // Send a fetch request to the /trending endpoint and return the top 3 results
 export const getTrendingGifs = async () => {
-
-}
+  const url = `${baseUrl}/trending?api_key=${API_KEY}&limit=3`;
+  try {
+    const [data, error] = await handleFetch(url);
+    if (error) throw error;
+    return data.data;
+  } catch (error) {
+    console.error('Error fetching trending gifs:', error);
+    return [];
+  }
+};
 
 // Send a fetch request to the /search endpoint with the given term as a query parameter
 export const getGifsBySearch = async (term) => {
-
-}
+  const url = `${baseUrl}/search?api_key=${API_KEY}&q=${term}&limit=3`;
+  try {
+    const [data, error] = await handleFetch(url);
+    if (error) throw error;
+    return data.data;
+  } catch (error) {
+    console.error('Error fetching gifs by search term:', error);
+    return [];
+  }
+};
--- app/src/playground.js
+++ app/src/playground.js
@@ -1,20 +1,27 @@
 // Import your adapter functions here
 
-import { handleFetch } from "./adapters/handleFetch.js";
+import { handleFetch } from './adapters/handleFetch.js';
 
 const testHandleFetch = async () => {
-  const [data, error] = await handleFetch('https://dog.ceo/api/breeds/image/random');
+  const [data, error] = await handleFetch(
+    'https://dog.ceo/api/breeds/image/random'
+  );
   if (error) {
     return console.log(error);
   }
   console.log(data);
-}
-
+};
 
 // Test your adapter functions here
 const testAdapters = async () => {
-
-}
+  const [data, error] = await handleFetch(
+    'https://dog.ceo/api/breeds/image/random'
+  );
+  if (error) {
+    return console.log(error);
+  }
+  console.log(data);
+};
 
 testHandleFetch();
 testAdapters();
